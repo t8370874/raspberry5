3ca1ff852918307a87b5e872838495a1a0c22c59
t8370874 <ycu123@gmail.com>
2026-03-28 15:10:29 +0800

    jj

--- lesson2/main.py
+++ lesson2/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from lesson2!")
+from google import genai
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="天空為什麼掉眼淚?"
+)
+print(response.text)
